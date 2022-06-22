48ce1f8d2f57f37a0915ba5831c1b88179545ff8
Eduardo <dudu_jr_007@hotmail.com>
2022-06-22 14:35:37 -0300

    sobre tipos primitivos e objetos

--- exercicios/src/fundamentos/PrimitivoVsObjeto.java
+++ exercicios/src/fundamentos/PrimitivoVsObjeto.java
@@ -5,11 +5,12 @@ public class PrimitivoVsObjeto
 	
 	public static void main(String[] args) 
 	{
-		
+		//Objeto
 		String s = new String("Texto");
 		s.toUpperCase();
 		
-		//Wrappers são a versão objeto dos tipos primitivos
+		//Obs: wrappers são a versão objeto dos tipos primitivos
+		//Primitivo
 		int num = 123;
 		System.out.println(num);
 		
