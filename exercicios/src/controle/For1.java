aef666094a7af558c05b8816fd84846958cc44dc
Eduardo <dudu_jr_007@hotmail.com>
2022-07-20 21:43:15 -0300

    estrutura de repetição For

---
+++ exercicios/src/controle/For1.java
@@ -0,0 +1,28 @@
+package controle;
+
+public class For1 
+{
+	
+	public static void main(String[] args) 
+	{	
+		
+		/*O laço de repetição for é o mais adequado quando o objetivo 
+		  é ter uma quantidade pré definida de vezes que o laço
+		  será repetido. Dentro dos parênteses o contador é declarado, 
+		  depois é definido um limite e por último o incremento ou 
+		  decremento*/
+		for(int contador = 0; contador <= 20; contador++)
+		{
+			System.out.println(contador);
+		}
+		
+		
+		/*Laço de repetição infinito*/
+//		for(;;)
+//		{
+//			System.out.println("Fim!");
+//		}
+		
+	}
+
+}
