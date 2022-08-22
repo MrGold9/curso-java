e741258d9fb57a45a4798635d3f30fb54204c3ed
Eduardo <dudu_jr_007@hotmail.com>
2022-08-22 18:28:17 -0300

    começando projeto de ExerciciosMatriz

---
+++ exercicios/src/avulsos/ExerciciosMatriz.java
@@ -0,0 +1,30 @@
+package avulsos;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class ExerciciosMatriz 
+{
+	
+	public static void main(String[] args) 
+	{
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		
+		Random aleatorio = new Random();
+		int linhas = aleatorio.nextInt(11);
+		int colunas = aleatorio.nextInt(11);
+
+		for (int i = 0; i <= linhas; i++) 
+		{
+			for (int j = 0; j <= colunas; j++) 
+			{
+				
+			}
+		}
+		
+		entrada.close();	
+	}
+	
+}
