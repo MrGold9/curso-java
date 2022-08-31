92c25d7c175ee35467a6cb7c2ffa7a5271b7ba3f
Eduardo <dudu_jr_007@hotmail.com>
2022-08-31 11:47:54 -0300

    Break rotulado e label no laço For

---
+++ exercicios/src/controle/BreakRotulado.java
@@ -0,0 +1,39 @@
+package controle;
+
+public class BreakRotulado 
+{
+	
+	public static void main(String[] args) 
+	{
+		/*
+		 	Colocamos uma label "externo" em um for e "interno"
+		 	no outro, a função do break rotulado é sair do
+		 	escopo indicado, ou seja, quando i for igual a 1, o
+		 	programa executará o break e ao invés de sair do escopo
+		 	do for interno, sairá do escopo do for externo,
+		 	retornando a mensagem final do código "Fim!" no terminal
+		 */
+		
+		externo: for (int i = 0; i < 3; i++) 
+		{
+			
+			interno: for (int j = 0; j < 3; j++) 
+			{
+				
+				if (i == 1) 
+				{
+					break externo;
+				}
+				
+				System.out.printf("[%d %d] ", i, j);
+			}
+			
+			System.out.println();
+			
+		}
+		
+		System.out.println("Fim!");
+		
+	}
+
+}
