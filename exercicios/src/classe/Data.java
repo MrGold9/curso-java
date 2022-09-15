214cf53eaa8924b04067e4cae352aee44e32518d
Eduardo <dudu_jr_007@hotmail.com>
2022-09-14 21:48:22 -0300

    Desafio Construtor

--- exercicios/src/classe/Data.java
+++ exercicios/src/classe/Data.java
@@ -5,6 +5,20 @@ public class Data
 	
 	String dia, mes, ano;
 	
+	Data()
+	{
+		dia = "01";
+		mes = "01";
+		ano = "1970";
+	}
+	
+	Data(String dia, String mes, String ano)
+	{
+		this.dia = dia;
+		this.mes = mes;
+		this.ano = ano;
+	}
+	
 	String dataFormatada() 
 	{
 		return dia + "/" + mes + "/" + ano;
--- exercicios/src/classe/DataTeste.java
+++ exercicios/src/classe/DataTeste.java
@@ -17,21 +17,34 @@ public class DataTeste
 		d2.mes = "03";
 		d2.ano = "2077";
 		
+		/*Atributos do construtor default da classe Produto serão mostrados*/
+		Data d3 = new Data();
 		
-		//Invocando o método dataFormatada para aglutinar
-		//os atributos e formar a data completa de cada objeto
-		String data1 = d1.dataFormatada();
-		String data2 = d2.dataFormatada();
+		/*Atributos definidos como parâmetros entre parênteses do 
+		objeto d4 serão mostrados*/
+		Data d4 = new Data("10", "10", "2010");
 		
-		//Output
-		System.out.println(data1);
-		System.out.println(data2);
+		
+		/*Invocando o método dataFormatada para aglutinar
+		os atributos e formar a data completa de cada objeto*/
+		//String data1 = d1.dataFormatada();
+		//String data2 = d2.dataFormatada();
+		
+		/*Output do objeto d1 e d2*/
+		//System.out.println(data1);
+		//System.out.println(data2);
+		
+		/*Invocando o método void imprimirDataFormatada*/
+		//d1.imprimirDataFormatada();
+		//d2.imprimirDataFormatada();
+		
+		/*Output do objeto d3*/
+		System.out.printf("%s/%s/%s", d3.dia, d3.mes, d3.ano);
 		
 		System.out.println();
 		
-		//Invocando o método void imprimirDataFormatada
-		d1.imprimirDataFormatada();
-		d2.imprimirDataFormatada();
+		/*Output do objeto d4*/
+		System.out.printf("%s/%s/%s", d4.dia, d4.mes, d4.ano);
 		
 	}
 
