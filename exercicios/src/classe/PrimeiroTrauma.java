4cf0b8f8e722e189cbc450775d44ebbabccf6d45
Eduardo <dudu_jr_007@hotmail.com>
2022-09-26 21:53:25 -0300

    Desafio Primeiro Trauma

---
+++ exercicios/src/classe/PrimeiroTrauma.java
@@ -0,0 +1,16 @@
+package classe;
+
+public class PrimeiroTrauma 
+{
+	
+	int a = 3; // não pode mexer aqui
+	
+	public static void main(String[] args) 
+	{
+		
+		double b = a;
+		System.out.println(b);
+		
+	}
+
+}
