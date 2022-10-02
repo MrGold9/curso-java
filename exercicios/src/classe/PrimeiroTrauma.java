4f5f3c2a566d3520f118ab82725b01b96ffb8d78
Eduardo <dudu_jr_007@hotmail.com>
2022-10-01 23:41:05 -0300

    Resolução do desafio

--- exercicios/src/classe/PrimeiroTrauma.java
+++ exercicios/src/classe/PrimeiroTrauma.java
@@ -4,11 +4,19 @@ public class PrimeiroTrauma
 {
 	
 	int a = 3; // não pode mexer aqui
+	static int b = 4;
 	
 	public static void main(String[] args) 
 	{
+		/*A variável "a" só pode ser chamada se for
+		 instanciado um objeto da classe em que está
+		 inserida*/
+		PrimeiroTrauma p = new PrimeiroTrauma();
+		System.out.println(p.a);
 		
-		double b = a;
+		
+		/*A variável pode ser chamada por qualquer 
+		 método pois está como static*/
 		System.out.println(b);
 		
 	}
