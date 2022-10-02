95beefffb8800406537f7f3c4895ac2a1be0214c
Eduardo <dudu_jr_007@hotmail.com>
2022-10-02 06:13:18 -0300

    sobre o this e o this()

--- exercicios/src/classe/Data.java
+++ exercicios/src/classe/Data.java
@@ -7,13 +7,22 @@ public class Data
 	
 	Data()
 	{
-		dia = "01";
-		mes = "01";
-		ano = "1970";
+		// dia = "01";
+		// mes = "01";
+		// ano = "1970";
+		
+		/*O this pode ser usado para invocar um
+		 outro construtor com os parâmetros 
+		 necessários e definir os valores dos 
+		 atributos de um objeto*/
+		this("01", "01", "1970");
 	}
 	
 	Data(String dia, String mes, String ano)
 	{
+		/*O this também serve para referenciar os 
+		atributos da classe e diferenciá-los dos 
+		parâmetros do método que possuem o mesmo nome*/
 		this.dia = dia;
 		this.mes = mes;
 		this.ano = ano;
