e6c62b96fbdfc1e588f03c4ca26afa48a35dc534
Eduardo <dudu_jr_007@hotmail.com>
2022-10-09 01:08:22 -0300

    início Desafio do Módulo

---
+++ exercicios/src/classe/Comida.java
@@ -0,0 +1,20 @@
+package classe;
+
+public class Comida 
+{
+	
+	String nomeComida = "";
+	static double pesoComida = 0.0;
+	
+	Comida()
+	{
+		
+	}
+	
+	Comida(String nomeComida, double pesoComida)
+	{
+		this.nomeComida = nomeComida;
+		this.pesoComida = pesoComida;
+	}
+
+}
