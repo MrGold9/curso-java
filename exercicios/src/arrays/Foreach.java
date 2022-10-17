9e79626c3a61a09e274434e75c57030e26028612
Eduardo <dudu_jr_007@hotmail.com>
2022-10-16 23:33:18 -0300

    FOR e FOREACH com array

---
+++ exercicios/src/arrays/Foreach.java
@@ -0,0 +1,28 @@
+package arrays;
+
+public class Foreach 
+{
+	
+	public static void main(String[] args) 
+	{
+		
+		//Declarando array
+		double[] notas = { 9.9, 8.7, 7.2, 9.4 };
+		
+		//Laço de repetição FOR
+		for (int i = 0; i < notas.length; i++) 
+		{
+			System.out.print(notas[i] + " ");
+		}
+		
+		System.out.println();
+		
+		//Laço de repetição FOREACH
+		for (double nota : notas) 
+		{
+			System.out.print(nota + " ");
+		}
+		
+	}
+
+}
