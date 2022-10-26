29236ee2dabaf7044da50b8d5beb202a7503679f
Eduardo <dudu_jr_007@hotmail.com>
2022-10-25 21:48:54 -0300

    adicionando comentários sobre matrizes

--- exercicios/src/arrays/Matriz.java
+++ exercicios/src/arrays/Matriz.java
@@ -17,15 +17,19 @@ public class Matriz
 		System.out.println("Quantas notas por aluno? ");
 		int qtdNotas = entrada.nextInt();
 		
+		//Criando uma matriz:
+		// [quantidade de arrays][quantidade de elementos por array]
 		double [][] notasDaTurma = new double[qtdAlunos][qtdNotas];
 		
 		double total = 0;
+		//Um FOR para percorrer os alunos
 		for (int a = 0; a < notasDaTurma.length; a++) 
 		{
+			//Um FOR para percorrer as notas
 			for (int n = 0; n < notasDaTurma[a].length; n++) 
 			{
 				
-				System.out.printf("Informe a nota do aluno %d do aluno %d: ",
+				System.out.printf("Informe a nota %d do aluno %d: ",
 						n + 1, a + 1);
 				notasDaTurma[a][n] = entrada.nextDouble();
 				total += notasDaTurma[a][n];
@@ -37,7 +41,11 @@ public class Matriz
 		double media = total / (qtdAlunos * qtdNotas);
 		System.out.println("Média da turma é " + media);
 		
-		System.out.println(Arrays.toString(notasDaTurma));
+		//FOREACH para mostrar as notas de cada aluno
+		for (double[] notasDoAluno : notasDaTurma) 
+		{
+			System.out.println(Arrays.toString(notasDoAluno));
+		}
 
 		entrada.close();
 		
