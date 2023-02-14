2d42667a1dbc2c13bd5b4688ee56f4e674c11306
Gold9 <dudu_jr_007@hotmail.com>
2023-02-13 22:03:53 -0300

    Main da classe Tempo1

---
+++ exercicios/src/oop/Tempo1Teste.java
@@ -0,0 +1,17 @@
+package oop;
+
+public class Tempo1Teste 
+{
+	
+	public static void main(String[] args) 
+	{
+		
+		Tempo1 tempo = new Tempo1();
+		
+		tempo.definirHora(10, 22, 50);
+		
+		System.out.println(tempo.toString());
+		
+	}
+
+}
