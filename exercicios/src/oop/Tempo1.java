beff3dc9d49e850ea7e806fdc531c282d638146d
Gold9 <dudu_jr_007@hotmail.com>
2023-02-14 22:00:09 -0300

    adicionando comentários na classe Tempo1

--- exercicios/src/oop/Tempo1.java
+++ exercicios/src/oop/Tempo1.java
@@ -10,10 +10,11 @@ public class Tempo1
 	public void definirHora(int hora, int minuto, int segundo) 
 	{
 		
+		//Erro caso a hora esteja incorreta
 		if (hora < 0 || hora >= 24 || minuto < 0 || minuto >= 60 || 
 				segundo < 0 || segundo >= 60) 
 		{
-			//
+			//Lançar exceção ou mensagem de erro através do throw
 			throw new IllegalArgumentException
 			("Hora, minuto ou segundo está fora do alcance");
 		}
@@ -29,6 +30,7 @@ public class Tempo1
 		return String.format("%02d:%02d:%02d", hora, minuto, segundo);
 	}
 	
+	//alterando o método padrão .toString para o padrão de hora
 	public String toString() 
 	{
 		return String.format("%d:%02d:%02d %s",
