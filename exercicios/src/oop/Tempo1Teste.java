2de15af8a2d06d50c92394a5855d439c2671aedb
Gold9 <dudu_jr_007@hotmail.com>
2023-02-23 21:57:55 -0300

    sobre métodos private

--- exercicios/src/oop/Tempo1Teste.java
+++ exercicios/src/oop/Tempo1Teste.java
@@ -1,5 +1,7 @@
 package oop;
 
+import java.sql.Time;
+
 public class Tempo1Teste 
 {
 	
@@ -8,17 +10,33 @@ public class Tempo1Teste
 		
 		Tempo1 tempo = new Tempo1();
 		
-		mostrarTempo("After time object is created", tempo);
+		mostrarTempo("Depois que o objeto tempo é criado", tempo);
 		System.out.println();
 		
+		tempo.definirTempo(13, 27, 6);
+		mostrarTempo("Depois de chamar o método definirTempo", tempo);
+		System.out.println();
 		
-		
-		private static void mostrarTempo(String cabecalho, Tempo1 t) 
+		try 
 		{
-			System.out.printf("%s%nUniversal time: %s%nStandard time: %s%n",
-					cabecalho, t.paraStringUniversal(), t.toString());
+			tempo.definirTempo(99, 99, 99); //valores fora do intervalo
+		}
+		catch(IllegalArgumentException e)
+		{
+			System.out.printf("Exceção: %s%n%n", e.getMessage());
 		}
 		
+		mostrarTempo("Depois da tentativa de definir valores inválidos", tempo);
+		
+		
+	}
+	
+	/*Métodos private só podem ser chamados dentro 
+	das classes em que foram criados*/
+	private static void mostrarTempo(String cabecalho, Tempo1 t) 
+	{
+		System.out.printf("%s%nTempo universal: %s%nTempo padrão: %s%n",
+				cabecalho, t.paraStringUniversal(), t.toString());
 	}
 
 }
