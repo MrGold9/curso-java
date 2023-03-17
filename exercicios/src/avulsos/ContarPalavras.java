d5667ccd00a86087492257d668dcad509e9292f1
Gold9 <dudu_jr_007@hotmail.com>
2023-03-16 22:59:57 -0300

    início desafio contador de palavras

---
+++ exercicios/src/avulsos/ContarPalavras.java
@@ -0,0 +1,26 @@
+package avulsos;
+
+public class ContarPalavras 
+{
+	
+	public static void main(String[] args) 
+	{
+		
+		
+		System.out.println(contarPalavras("Oi"));
+		
+		
+	}
+	
+	
+	
+	public static int contarPalavras(String palavras) 
+	{
+		int numPalavras = 0;
+		
+		
+		
+		return numPalavras;
+	}
+
+}
