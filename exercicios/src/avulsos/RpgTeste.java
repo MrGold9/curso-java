e37a85bb7239dcc208661e694e24210e6ee62158
MrGold9 <dudu_jr_007@hotmail.com>
2023-03-27 13:08:20 -0300

    loop while e teste de métodos

--- exercicios/src/avulsos/RpgTeste.java
+++ exercicios/src/avulsos/RpgTeste.java
@@ -1,11 +1,43 @@
 package avulsos;
 
-public class RpgTeste {
+import java.util.Scanner;
+
+public class RpgTeste 
+{
 
 	public static void main(String[] args) 
 	{
+		Scanner entrada = new Scanner(System.in);
+		String continuar = "s";
 		
-		// TODO Auto-generated method stub
+		while(continuar == "s")
+		{
+		
+		RpgJogador jogador1 = new RpgJogador("Walter");
+		RpgInimigo inimigo1 = new RpgInimigo();
+		
+		System.out.println(jogador1.nomeJogador + "\nVida: " + jogador1.vidaJogador);
+		System.out.println();
+		System.out.println(inimigo1.nomeInimigo + "\nVida: " + inimigo1.vidaInimigo);
+		System.out.println();
+		
+		
+		System.out.println();
+
+		
+		if (inimigo1.vidaInimigo <= 0) 
+		{
+			System.out.println(jogador1.nomeJogador + " ganhou o jogo");
+		}
+		else if (jogador1.vidaJogador <= 0) 
+		{
+			System.out.println(inimigo1.nomeInimigo + " ganhou o jogo");
+		}
+		
+		
+		//continuar = entrada.nextLine();
+		
+		}
 
 	}
 
