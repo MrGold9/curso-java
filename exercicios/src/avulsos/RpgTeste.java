e3b0669119409e0591bfd916db53eeae9f21e1d3
MrGold9 <dudu_jr_007@hotmail.com>
2023-03-28 15:26:17 -0300

    criando ações do personagem

--- exercicios/src/avulsos/RpgTeste.java
+++ exercicios/src/avulsos/RpgTeste.java
@@ -9,57 +9,66 @@ public class RpgTeste
 	{
 		Scanner entrada = new Scanner(System.in);
 		char continuar = 's';
-		int acao;
 		
 		while (continuar == 's')
 		{
+			int acao = 0;
 		
-		RpgJogador jogador1 = new RpgJogador("Walter");
-		RpgInimigo inimigo1 = new RpgInimigo();
+			RpgJogador jogador1 = new RpgJogador("Walter");
+			RpgInimigo inimigo1 = new RpgInimigo();
 		
-		System.out.println(jogador1.nomeJogador + "\nVida: " + jogador1.vidaJogador);
-		System.out.println();
-		System.out.println(inimigo1.nomeInimigo + "\nVida: " + inimigo1.vidaInimigo);
-		System.out.println();
-		
-		switch (acao) 
-		{
-		case 1: 
-		{
+			System.out.println(jogador1.nomeJogador + "\nVida: " + jogador1.vidaJogador);
+			System.out.println();
+			System.out.println(inimigo1.nomeInimigo + "\nVida: " + inimigo1.vidaInimigo);
+			System.out.println();
 			
-		}
-		case 2:
-		{
-			
-		}
-		default:
-		{
-			throw new IllegalArgumentException("Unexpected value: ");
-		}
+			System.out.println("Selecione uma ação: \n1 - Golpear \n2 - Defender");
+		
+			acao = entrada.nextInt();
+			switch (acao)
+			{
+				case 1:
+				{
+					jogador1.golpearInimigo();
+					break;
+				}
+		
+				case 2:
+				{
+					System.out.println("2");
+					break;
+				}
+		
+				default:
+				{
+					throw new IllegalArgumentException("Insira uma opção válida");
+				}
+		
+			}
 		
 		
-		System.out.println();
+			System.out.println();
 
 		
-		if (inimigo1.vidaInimigo <= 0) 
-		{
-			System.out.println(jogador1.nomeJogador + " ganhou o jogo");
+			if (inimigo1.vidaInimigo <= 0) 
+			{
+				System.out.println(jogador1.nomeJogador + " ganhou o jogo");
+			}
+			else if (jogador1.vidaJogador <= 0) 
+			{
+				System.out.println(inimigo1.nomeInimigo + " ganhou o jogo");
+			}
+		
+			System.out.println("Deseja reiniciar o jogo? (s/n)");
+			continuar = entrada.next().charAt(0);
+		
+		
+		
+
 		}
-		else if (jogador1.vidaJogador <= 0) 
-		{
-			System.out.println(inimigo1.nomeInimigo + " ganhou o jogo");
-		}
-		
-		System.out.println("Deseja reiniciar o jogo? (s/n)");
-		continuar = entrada.next().charAt(0);
-		
-		
-		}
-		
-		System.out.println("Jogo finalizado");
 		
 		entrada.close();
-
+		System.out.println("Jogo finalizado");
 	}
-
 }
+
